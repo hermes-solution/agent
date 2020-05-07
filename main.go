7d5cc607e96048a6c5a4eae66da15b5392f07239
Loc Ngo <locngo@fortna.com>
2020-05-07 09:52:30 +0700

    Add etcd

--- main.go
+++ main.go
@@ -1,34 +1,38 @@
 package main
 
 import (
+	"context"
 	"crypto/md5"
 	"flag"
 	"fmt"
+	"go.etcd.io/etcd/clientv3"
 	"io"
+	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"os/exec"
 	"os/signal"
+	"strings"
 	"time"
 )
 
 /**
-agent --use-file --use-api --use-etcd --port --bind
+agent --use-file --file --use-etcd --etcd-address
  */
 const (
 	fluentdCommand = "fluentd"
-	fluentdConfig  = "/fluentd/etc/fluentd.conf"
+	fluentdConfig  = "/hermes/fluentd.conf"
+	originalConfig = "/hermes/forward.conf"
 )
 
 var (
-	useFile         = false
-	intervalRefresh = 1000
-	useApi          = false
-	useEtcd         = false
-	apiPort         = 80
-	fileChecksum    = ""
-	cmd             *exec.Cmd
+	useFile      = false
+	fileConfig   = ""
+	useEtcd      = false
+	etcdAddress  = ""
+	etcdKey      = ""
+	fileChecksum = ""
+	cmd          *exec.Cmd
 )
 
 func hashMD5(file string) (string, error) {
@@ -46,84 +50,110 @@ func hashMD5(file string) (string, error) {
 	return fmt.Sprintf("%x", hasher.Sum(nil)), nil
 }
 
-func checksumAndReload() {
+func start() {
+	var err error
+	if cmd != nil {
+		err = KillProcess(cmd.Process.Pid)
+		if err != nil {
+			log.Println("kill old process get error", err)
+		} else {
+			log.Println("kill old process successful")
+		}
+	}
+	args := make([]string, 0)
+	args = append(args, "-c")
+	args = append(args, fluentdConfig)
+	err = os.Setenv("FLUENTD_CONF", "fluentd.conf")
+	cmd = exec.Command(fluentdCommand, args...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if cmd.Env == nil {
+		cmd.Env = make([]string, 0)
+	}
+	cmd.Env = append(cmd.Env, "FLUENTD_CONF=fluentd.conf")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checksumAndStart() {
 	checksum, err := hashMD5(fluentdConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
 	if fileChecksum == "" || fileChecksum != checksum {
-		if cmd != nil {
-			err = KillProcess(cmd.Process.Pid)
-			if err != nil {
-				log.Println("kill old process get error", err)
-			} else {
-				log.Println("kill old process successful")
-			}
-		}
-		args := make([]string, 0)
-		args = append(args, "-c")
-		args = append(args, fluentdConfig)
-		err = os.Setenv("FLUENTD_CONF", "fluentd.conf")
-		cmd = exec.Command(fluentdCommand, args...)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if cmd.Env == nil {
-			cmd.Env = make([]string, 0)
-		}
-		cmd.Env = append(cmd.Env, "FLUENTD_CONF=fluentd.conf")
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-		err = cmd.Start()
-		if err != nil {
-			log.Fatal(err)
-		}
+		start()
 		fileChecksum = checksum
 	}
 }
 
+func rewriteConfig(appendData []byte) error {
+	data, err := ioutil.ReadFile(originalConfig)
+	if err != nil {
+		return err
+	}
+
+	if appendData != nil && len(appendData) > 0 {
+		data = append(data, []byte("\n")...)
+		data = append(data, appendData...)
+	}
+	log.Println("new configuration: ", string(data))
+	err = ioutil.WriteFile(fluentdConfig, data, 0777)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func main() {
 	log.SetOutput(os.Stdout)
-	flag.BoolVar(&useFile, "use-file", true, "")
-	flag.IntVar(&intervalRefresh, "interval-refresh", 1000, "")
-	flag.BoolVar(&useApi, "use-api", false, "")
-	flag.BoolVar(&useEtcd, "use-etcd", false, "")
-	flag.IntVar(&apiPort, "api-port", 80, "")
+	flag.BoolVar(&useFile, "use-file", false, "using file config is passed via file argument")
+	flag.StringVar(&fileConfig, "file", "", "absolute path of configuration file")
+	flag.BoolVar(&useEtcd, "use-etcd", false, "using configuration is fetched from etcd")
+	flag.StringVar(&etcdAddress, "etcd-address", "", "address of etcd")
+	flag.StringVar(&etcdKey, "etcd-key", "/hermes/agent/config", "configuration key in etcd")
 	flag.Parse()
-	if useFile {
-		checksumAndReload()
-		go func() {
-			t := time.NewTicker(time.Duration(intervalRefresh) * time.Millisecond)
-			for {
-				<-t.C
-				checksumAndReload()
-			}
-		}()
-	}
-	if useApi {
-		http.HandleFunc("/config", func(writer http.ResponseWriter, request *http.Request) {
-			if request.Method == "POST" {
-			} else if request.Method == "GET" {
-			} else {
-			}
-			writer.Header().Set("Content-Type", "application/json")
-			writer.Header().Set("Server", "hermes-agent")
-			writer.WriteHeader(200)
-			_, _ = writer.Write([]byte("{\"code\":200,\"message\":\"OK\"}"))
-		})
-		go func() {
-			err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", apiPort), nil)
-			if err != nil {
-				log.Fatal(err)
-			}
-		}()
+
+	if useEtcd && etcdAddress != "" && etcdKey != "" {
+		useFile = false
+		fileConfig = ""
 	}
 
-	if useEtcd {
-		go func() {
+	if useFile && fileConfig != "" {
+		matchData, err := ioutil.ReadFile(fileConfig)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-		}()
+		err = rewriteConfig(matchData)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go watchOnConfigFile()
 	}
+	if useEtcd && etcdAddress != "" && etcdKey != "" {
+		matchData, err := readConfigFromEtcd()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = rewriteConfig(matchData)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go watchOnEtcdConfigChange()
+	}
+	/**
+	start fluentd and scheduler for reloading
+	 */
+	checksumAndStart()
+	go scheduleReload()
 
 	signalChannel := make(chan os.Signal, 1)
 	signal.Notify(signalChannel, GetSingal()...)
@@ -136,6 +166,91 @@ func main() {
 				log.Println(err)
 			}
 		}
+		close(signalChannel)
 		break
+	}
+}
+
+func readConfigFromEtcd() ([]byte, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   strings.Split(etcdAddress, ","),
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = cli.Close()
+	}()
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer func() {
+		cancel()
+	}()
+	response, err := cli.Get(ctx, etcdKey)
+	if err != nil {
+		return nil, err
+	}
+	if response.Kvs != nil && len(response.Kvs) > 0 {
+		for _, v := range response.Kvs {
+			return v.Value, nil
+		}
+	}
+	return nil, nil
+}
+
+func watchOnEtcdConfigChange() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   strings.Split(etcdAddress, ","),
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		_ = cli.Close()
+	}()
+
+	rch := cli.Watch(context.Background(), etcdKey)
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			err = rewriteConfig(ev.Kv.Value)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func watchOnConfigFile() {
+	originalSum, err := hashMD5(fileConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := time.NewTicker(2 * time.Second)
+	for {
+		<-t.C
+		newSum, err := hashMD5(fileConfig)
+		if err != nil {
+			log.Println("READ CONFIG FILE GET ERROR: ", err)
+		}
+		if newSum != originalSum {
+			originalSum = newSum
+			matchData, err := ioutil.ReadFile(fileConfig)
+			if err != nil {
+				log.Fatal(err)
+			}
+			err = rewriteConfig(matchData)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func scheduleReload() {
+	t := time.NewTicker(5000 * time.Millisecond)
+	for {
+		<-t.C
+		checksumAndStart()
 	}
 }
